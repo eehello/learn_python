483f0812196e0e9e328a824d30f47f8ad920260f
eehello <ldf3699@qq.com>
2015-09-10 23:11:25 +0800

    a add

--- quadratic.py
+++ quadratic.py
@@ -10,6 +10,9 @@ def quadratic(a,b,c):
 w1=int(input("请输入a的值:"))
 w2=int(input("请输入b的值:"))
 w3=int(input("请输入c的值:"))
-
-x, y = quadratic(w1,w2,w3)
-print("一元二次方程的一个解为：%f ,另一个解为：%f 。" % x,y)
+f_ac=(w2*w2-4*w1*w3)
+if  f_ac > 0:
+    x, y = quadratic(w1,w2,w3)
+    print("一元二次方程的一个解为：%f ,另一个解为：%f 。" % (x,y))
+else :
+    print("b*b-4*a*c不大于0")
