e5aed1e7b8dd28ddaa44ec61ccaed6d8dd56635c
eehello <ldf3699@qq.com>
2016-04-11 22:28:49 +0800

    4.11

--- oldboy/day2/day2_zuoye/gou_wu_che/gwc.py
+++ oldboy/day2/day2_zuoye/gou_wu_che/gwc.py
@@ -2,5 +2,12 @@
 # coding=utf-8
 # Copyright © 2016 ldf@eehello.com All Rights Reserved
 # 本程序在python3.4下测试通过
-shop = [tv:3000,iphone:5000,tea:10,move:35,spring:1200,fangte:200,
-        shopping:500]
+shops = {'tv':3000,'iphone':5000,'tea':10,'move':35,'spring':1200,
+         'fangte':200,'shopping':500}
+
+gongzi = int(input('请输入你的工资：'))
+
+print('物品清单为：')
+for i in shops:
+    print('物品',i,',价格为：',shops[i],'元')
+print('你的可用金额为：',gongzi)
