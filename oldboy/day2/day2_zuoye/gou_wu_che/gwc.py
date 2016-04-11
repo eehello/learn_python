a088ac8d09fd92764beb96f2e1ddd2fc9b6cf9d1
eehello <ldf3699@qq.com>
2016-04-11 11:26:51 +0800

    day2zuoye

---
+++ oldboy/day2/day2_zuoye/gou_wu_che/gwc.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright © 2016 ldf@eehello.com All Rights Reserved
+# 本程序在python3.4下测试通过
+shop = [tv:3000,iphone:5000,tea:10,move:35,spring:1200,fangte:200,
+        shopping:500]
---
+++ oldboy/day2/day2_zuoye/name_list/name_search.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright © 2016 ldf@eehello.com All Rights Reserved
+# 本程序在python3.4下测试通过
