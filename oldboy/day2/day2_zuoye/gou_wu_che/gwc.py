031e378a05b8e9cbc71a077f819dacd67b932b2d
eehello <ldf3699@qq.com>
2016-04-12 10:06:02 +0800

    4.12

--- oldboy/day2/day2_zuoye/gou_wu_che/gwc.py
+++ oldboy/day2/day2_zuoye/gou_wu_che/gwc.py
@@ -7,7 +7,25 @@ shops = {'tv':3000,'iphone':5000,'tea':10,'move':35,'spring':1200,
 
 gongzi = int(input('请输入你的工资：'))
 
-print('物品清单为：')
-for i in shops:
-    print('物品',i,',价格为：',shops[i],'元')
-print('你的可用金额为：',gongzi)
+buys = []
+min_shop = gongzi
+
+while gongzi >= min_shop:
+    print('物品清单为：')
+    shops_value = []
+    for i in shops:
+        print('物品:%s,价格为:%d元' % (i,shops[i]))
+        shops_value.append(shops[i])
+    print('----------------------------------------------------------')
+    print('你的可用金额为：',gongzi)
+    min_shop = min(shops_value)
+    buy = str(input('请输入你想购买的物品名称(退出请按字母 q ):'))
+    if buy == 'q':
+        exit()
+    gongzi = gongzi - shops[buy]
+    buys.append(buy)
+
+print('你购买了如下产品:')
+for i in buys:
+    print(i)
+print('你的余额为:%d' % gongzi)
--- oldboy/day2/day2_zuoye/name_list/name_search.py
+++ oldboy/day2/day2_zuoye/name_list/name_search.py
@@ -2,3 +2,23 @@
 # coding=utf-8
 # Copyright © 2016 ldf@eehello.com All Rights Reserved
 # 本程序在python3.4下测试通过
+
+ok = []
+name_dict = {}
+
+search_name = str(input('请输入你要查询的用户:'))
+
+f_name = open('name_list.cfg' , 'r')
+for name in f_name:
+    name = name.strip().split(':')
+    name_dict[name[0]] = name[1]
+    if search_name in name[0]:
+        ok.append(name[0])
+
+f_name.close()
+
+print('符合查询条件有%d位\n 分别为:' % len(ok))
+
+for i in name_dict:
+    print('%s:%s' % (i,name_dict[i])
+   
