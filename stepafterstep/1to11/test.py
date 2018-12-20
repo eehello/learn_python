8e4540ef3f5bc638a18b83286b30caa8c5bc4799
ldf <ldf3699@qq.com>
2018-12-21 07:08:52 +0800

    111

---
+++ stepafterstep/1to11/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+# @Created on : 2018/12/21 6:11
+# @Author     : ldf (ldf@eehello.com)
+# @Link       : http://www.eehello.com
+# @Version    : 
+# 本程序在python2.7下测试通过
+"""
+
+list = [1,2,3,4,5,6,7]
+
+for i in list:
+    print(i)
+print("It is end int:" + i)
+
---
+++ stepafterstep/two/name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+# @Created on : 2018/12/20 15:45
+# @Author     : ldf (ldf@eehello.com)
+# @Link       : http://www.eehello.com
+# @Version    : 
+# 本程序在python2.7下测试通过
+"""
+
+
+name = "aDa lovE lilei"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("\t"+name.title())
+
+a = "language:\n\tpython\nC\njave"
+print(a)
