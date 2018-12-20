858a290833cc6118d9f1aaf7a968328dc54fa33e
ldf <ldf3699@qq.com>
2018-12-20 14:53:03 +0800

    111

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 这是一个学习python及git做的练习代码目录。
 2016年4月14日从github.com 迁入 git.oschina.net
 
+2018年12月20日，为了以后更广的眼界，迁回github
---
+++ stepafterstep/one/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+# @Created on : 2018/12/20 11:41
+# @Author     : ldf (ldf@eehello.com)
+# @Link       : http://www.eehello.com
+# @Version    : 
+# 本程序在python2.7下测试通过
+"""
+
+print("Hello world!")
+print "Hello world! python2"
