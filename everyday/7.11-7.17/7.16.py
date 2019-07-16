dfdf5294884850189bab118e7d0a38f28e7a5236
ldf <admin@eehello.com>
2019-07-16 16:03:33 +0800

    7.16

---
+++ everyday/7.11-7.17/7.16.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding=utf-8
+"""
+# @Created on : 2019/07/16 16:01
+# @Author     : ldf (ldf@eehello.com)
+# @Link       : http://www.eehello.com
+# @Version    : 
+# 本程序在python3.4下测试通过
+"""
+
+
+list01 = [1,2,3,4,5]
+print("列表第三个元素为：",list01[2])
